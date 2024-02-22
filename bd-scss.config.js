ae6d1e8c2343413d79d96a389041560b4be4208f
Riddim-GLiTCH <riddim_glitch@proton.me>
2024-02-22 16:11:59 +0100

    Initialize theme

---
+++ bd-scss.config.js
@@ -0,0 +1,10 @@
+/** @type {import('bd-scss/lib/config').Config} */
+export default {
+	meta: {
+		name: "EnhancedLinks",
+		author: "Riddim-GLiTCH",
+		version: "1.0.0",
+		description: "a Mini theme that enhances the appearance of Links in Discord",
+		source: "https://github.com/Nexus-Themes/EnhancedLinks"
+	}
+}
